1d30737a9c1a1ebcf1f808b53aa1035d464a1cb5
syzer3@gmail.com <syzer3@gmail.com>
2016-09-17 09:16:58 +0200

    initial setup x2

--- getBayesSentiment.js
+++ getBayesSentiment.js
@@ -1,11 +1,8 @@
-/**
- * Created by giga on 17/09/16.
- */
-var level = require('level');
-var bayes = require('syzer-level-naive-bayes');
+var level = require('level')
+var bayes = require('syzer-level-naive-bayes')
 
-var db = level('./mydb');
+var db = level('./mydb')
 var nb = bayes(db); // where db is a levelup instance
-nb.classify('awesome, cool, amazing!! Yay.', function(err, category) {
-    console.log('category is '+category)
-});
+nb.classify('awesome, cool, amazing!! Yay.', (err, category) => {
+    console.log('category is ' + category)
+})
