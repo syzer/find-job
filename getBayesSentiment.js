7042e5ea27ea0c5ebcf1390349d951079244d0fc
syzer3@gmail.com <syzer3@gmail.com>
2016-09-17 15:20:43 +0200

    train bayes and get bayes

--- getBayesSentiment.js
+++ getBayesSentiment.js
@@ -1,11 +1,12 @@
 /**
  * Getting sentiment from a pre-trained Bayesian classifier.
  */
-var level = require('level');
-var bayes = require('syzer-level-naive-bayes');
+let level = require('level')
+let bayes = require('syzer-level-naive-bayes')
 
-var db = level('./mydb');
-var nb = bayes(db); // where db is a levelup instance
-nb.classify('awesome, cool, amazing!! Yay.', function(err, category) {
-    console.log('category is '+category)
-});
+let db = level('./database/sentiment')
+let nb = bayes(db)
+
+nb.classifyAsync('awesome, cool, amazing!! Yay.')
+  .then(console.log)
+  .catch(console.error)
--- trainbayes.js
+++ trainbayes.js
@@ -5,12 +5,12 @@
 let level = require('level')
 let bayes = require('syzer-level-naive-bayes')
 
-let db = level('./mydb')
+let db = level('./database/sentiment')
 
-let nb = bayes(db) // where db is a levelup instance
+let nb = bayes(db)
 
 let lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('traindata.txt')
+    input: require('fs').createReadStream('./database/traindata.txt')
 })
 
 lineReader.on('line', function (line) {
