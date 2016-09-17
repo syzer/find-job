5bbc1bc781e0ff37ce074cd1b59f7817597de277
Ivan Nesic <fonposta@gmail.com>
2016-09-17 11:08:06 +0200

    added fullcontact search

---
+++ fullcontactSearch.js
@@ -0,0 +1,17 @@
+/**
+ * Using fullcontact to mine the data about the person based on email
+ */
+console.log(fullcontactSearch("syzer3@gmail.com"));
+
+function fullcontactSearch(email) {
+
+  var fullcontact = require('fullcontact').createClient('ef9002ff700b7a');
+  fullcontact.person.email(email, function (err, data) {
+    //console.log(data);
+    return data;
+  });
+}
+
+//curl -H "X-FullContact-APIKey: ef9002ff700b7a" https://api.fullcontact.com/v2/person.json?email=syzer3%40gmail.com
+
+//ef9002ff700b7a
