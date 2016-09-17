b68cdd95cc50ec1ed4e163d3c6f838e3e29112ee
syzer3@gmail.com <syzer3@gmail.com>
2016-09-17 09:32:31 +0200

    removed ;

--- app/app.js
+++ app/app.js
@@ -1,17 +1,17 @@
-import angular from 'angular';
-import ngAnimate from 'angular-animate';
-import ngMessages from 'angular-messages';
-import ngCookies from 'angular-cookies';
-import ngResource from 'angular-resource';
-import ngSanitize from 'angular-sanitize';
-import ngMaterial from 'angular-material';
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import angular from 'angular'
+import ngAnimate from 'angular-animate'
+import ngMessages from 'angular-messages'
+import ngCookies from 'angular-cookies'
+import ngResource from 'angular-resource'
+import ngSanitize from 'angular-sanitize'
+import ngMaterial from 'angular-material'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
-import rootComponent from './root';
-import messagesComponent from './messages/messages.component.js';
-import MessageService from './messages/messages.service.js';
-import rumorComponent from './rumors/rumors.component.js';
-import RumorService from './rumors/rumors.service.js';
+import rootComponent from './root'
+import messagesComponent from './messages/messages.component.js'
+import MessageService from './messages/messages.service.js'
+import rumorComponent from './rumors/rumors.component.js'
+import RumorService from './rumors/rumors.service.js'
 
 require('angular-material/angular-material.css')
 
